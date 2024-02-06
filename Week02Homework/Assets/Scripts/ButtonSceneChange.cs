80926a9a46a80a8d3bce64b6485441a0cd223ca2
NicholeShinn <157531558+NicholeShinn@users.noreply.github.com>
2024-02-06 09:47:37 -0500

    MainGameCreation
    First Level Creations

---
+++ Week02Homework/Assets/Scripts/ButtonSceneChange.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSceneChange : MonoBehaviour
+{
+
+  public void IntroButton() //function to use on click event 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load Scene through getting the current scene and continuing by 1 in the build settings index
+    }
+}
